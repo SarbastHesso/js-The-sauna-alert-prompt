8aa4c8b8149d12083114d5e6887f0013920b1a6e
Sarbast Hesso <sarbast.hesso@gmail.com>
2024-01-17 14:29:18 +0100

    the sauna with alert and prompt

---
+++ script.js
@@ -0,0 +1,21 @@
+const temperatureController = () => {
+  let temperatureInFahrenheit = prompt('Pleas, enter the temperature in Fahrenheit:');
+
+  while (temperatureInFahrenheit){
+    let temperatureInCelsius = parseInt(((temperatureInFahrenheit - 32) * 5) / 9);
+    if (temperatureInCelsius <= 77 && temperatureInCelsius >= 73) {
+      alert(`Sauna temperature is: ${temperatureInFahrenheit} fahrenheit. Enjoy!`);
+      break;
+    } else if (temperatureInCelsius < 73) {
+      alert(`The sauna is cold, the temperature is: ${temperatureInFahrenheit} fahrenheit, turn up the heat a bit`);
+    } else if (temperatureInCelsius > 77) {
+      alert(`The sauna is hot, the temperature is: ${temperatureInFahrenheit} fahrenheit, turn down the heat a bit`);
+    } else {
+      alert(`Something went Wrong, please enter the temperature in a correct form`);
+    }
+
+    temperatureInFahrenheit = prompt("Pleas, Set a new temperature in Fahrenheit:");
+  }
+}
+
+temperatureController();
